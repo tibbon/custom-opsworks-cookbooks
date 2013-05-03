251d38c9aa82235e7ed8effe58d7d3f62050eb57
David Fisher <tibbon+github@gmail.com>
2013-05-03 16:02:04 -0400

    Relaxed postgres sercurity settings

--- postgresql/attributes/default.rb
+++ postgresql/attributes/default.rb
@@ -175,8 +175,9 @@ end
 default['postgresql']['pg_hba'] = [
   {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
   {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
-  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
-  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+  #{:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
+ # {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '0.0.0.0/0', :method => 'md5'}
 ]
 
 default['postgresql']['password'] = Hash.new
